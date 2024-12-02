bb6ce8d7a815456310093570bbca9b9b349d9bb4
Jessica Clarensia S <113441688+tootzie@users.noreply.github.com>
2024-12-02 15:48:48 +0700

    office 1

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import styles from "../ui/dashboard/dashboard.module.css";
+import Card from "../ui/dashboard/card/card";
+import Transaction from "../ui/dashboard/transaction/transaction";
+import Graph from "../ui/dashboard/graph/graph";
+import Rightbar from "../ui/dashboard/rightbar/rightbar";
 
-const LoginPage = () => {
-  return <div>Dashboard Page</div>;
+const DashboardPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.main}>
+        <div className={styles.cards}>
+          <Card />
+          <Card />
+          <Card />
+        </div>
+        <Transaction />
+        <Graph />
+      </div>
+      <div className={styles.side}>
+        <Rightbar />
+      </div>
+    </div>
+  );
 };
 
-export default LoginPage;
+export default DashboardPage;
